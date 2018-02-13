ef99699ce4c25eb6ac56dda169b46f0a72c00e7b
donezio <donezio32@gmail.com>
2018-02-13 15:09:43 -0800

    n

--- sleepRecorder/Controller/recorderControler.swift
+++ sleepRecorder/Controller/recorderControler.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBAction func press(_ sender: Any) {
+        let button = sender as! UIButton
+        button.setTitle("i am pressed", for: .normal)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startRecording(){
+        
+    }
 }
 
